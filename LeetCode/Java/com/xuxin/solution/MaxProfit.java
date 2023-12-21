b087ac01b770be3935cfcce17d4f7d2b34dd57de
xuxin <2860543348@qq.com>
2023-12-21 19:04:56 +0800

    Leetcode121.买卖股票的最佳时机

---
+++ LeetCode/Java/com/xuxin/solution/MaxProfit.java
@@ -0,0 +1,17 @@
+package com.xuxin.solution;
+
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+}
