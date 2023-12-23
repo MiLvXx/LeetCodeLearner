25caaae482c6c28b0149a8d573118ed487ebcfca
xuxin <2860543348@qq.com>
2023-12-23 12:46:26 +0800

    Leetcode55.跳跃游戏

---
+++ LeetCode/Java/com/xuxin/solution/CanJump.java
@@ -0,0 +1,14 @@
+package com.xuxin.solution;
+
+public class CanJump {
+
+    public boolean canJump(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k)
+                return false;
+            k = Math.max(k, i + nums[i]);
+        }
+        return true;
+    }
+}
