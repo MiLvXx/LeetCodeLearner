6eabd48b82455da1a5567179e5b47c7df8c24d5c
xuxin <2860543348@qq.com>
2024-01-10 23:15:29 +0800

    Leetcode14.最长公共前缀

---
+++ LeetCode/Java/com/xuxin/solution/LongestCommonPrefix.java
@@ -0,0 +1,29 @@
+package com.xuxin.solution;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+
+        String prefix = strs[0];
+        int count = strs.length;
+
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public String longestCommonPrefix(String str1, String str2) {
+        int len = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < len && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+    
+}
