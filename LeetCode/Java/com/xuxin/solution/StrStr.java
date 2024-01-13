cec804bb4e347d673f0fe898c8903a818ab92cd0
xuxin <2860543348@qq.com>
2024-01-13 14:09:25 +0800

    Leetcode28.找出字符串中第一个匹配项的下标

---
+++ LeetCode/Java/com/xuxin/solution/StrStr.java
@@ -0,0 +1,21 @@
+package com.xuxin.solution;
+
+public class StrStr {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
