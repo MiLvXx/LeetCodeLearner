f60122fa61b86a403cefbc7eb7b29001f00c095a
xuxin <2860543348@qq.com>
2024-01-31 11:02:49 +0800

    Leetcode49.字母异位词分组

---
+++ LeetCode/Java/com/xuxin/solution/GroupAnagrams.java
@@ -0,0 +1,28 @@
+package com.xuxin.solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        new GroupAnagrams()
+            .groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"})
+            .forEach(str -> System.out.println(str));
+    }
+}
