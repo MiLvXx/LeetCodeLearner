ac52107a92e648aaf598e1f9fd1eaacd677dfb48
xuxin <2860543348@qq.com>
2024-02-04 22:41:27 +0800

    Leetcode228.汇总区间

---
+++ LeetCode/Java/com/xuxin/solution/SummaryRanges.java
@@ -0,0 +1,26 @@
+package com.xuxin.solution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+    public List<String> summaryRanges(int[] nums) {
+        int head = 0, tail = 0;
+        List<String> res = new ArrayList<>();
+        while (tail < nums.length) {
+            if (tail + 1 == nums.length || nums[tail + 1] != nums[tail] + 1) {
+                res.add("" + nums[head] +
+                        (head == tail ? "" : "->" + nums[tail]));
+                head = tail + 1;
+            }
+            tail++;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        new SummaryRanges()
+                .summaryRanges(new int[] {})
+                .forEach(ele -> System.out.println(ele));
+    }
+}
