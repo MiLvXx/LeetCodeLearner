4b11f1082016e4583b7a787985d745929565b7f5
xuxin <2860543348@qq.com>
2024-02-12 10:59:40 +0800

    Leetcode155.最小栈

---
+++ LeetCode/Java/com/xuxin/solution/MinStack.java
@@ -0,0 +1,33 @@
+package com.xuxin.solution;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MinStack {
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<>();
+        minStack = new LinkedList<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        xStack.push(x);
+        minStack.push(Math.min(minStack.peek(), x));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
