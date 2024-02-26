60af64b67c30280e28283b2b5200e9172301efde
xuxin <2860543348@qq.com>
2024-02-27 00:59:41 +0800

    Remove main func

--- LeetCode/Java/com/xuxin/solution/Partition.java
+++ LeetCode/Java/com/xuxin/solution/Partition.java
@@ -20,13 +20,4 @@ public class Partition {
         minNode.next = pMaxNode.next;
         return pMinNode.next;
     }
-
-    public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(4);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(2);
-
-        new Partition().partition(head, 3);
-    }
 }
