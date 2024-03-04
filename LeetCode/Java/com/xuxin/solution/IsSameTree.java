a2e6a533f6203f5003b2654feda8804401b6a17f
xuxin <2860543348@qq.com>
2024-03-04 08:34:51 +0800

    Leetcode100.相同的树

---
+++ LeetCode/Java/com/xuxin/solution/IsSameTree.java
@@ -0,0 +1,17 @@
+package com.xuxin.solution;
+
+import com.xuxin.util.TreeNode;
+
+public class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
