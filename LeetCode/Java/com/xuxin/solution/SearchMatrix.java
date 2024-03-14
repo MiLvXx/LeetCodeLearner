3c2a857d2f262beca316d00e479f8dbdba0a163a
xuxin <2860543348@qq.com>
2024-03-14 13:43:44 +0800

    Leetcode240.搜索二维矩阵Ⅱ

---
+++ LeetCode/Java/com/xuxin/solution/SearchMatrix.java
@@ -0,0 +1,20 @@
+package com.xuxin.solution;
+
+public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int row = 0, col = matrix[0].length-1;
+        while (row < matrix.length && col >= 0) {
+            if (target > matrix[row][col]) {
+                row++;
+            } else if (target < matrix[row][col]) {
+                col--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
