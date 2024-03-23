769eb5972644562d5587e9f63f68860ae9cae499
xuxin <2860543348@qq.com>
2024-03-23 11:11:37 +0800

    Leetcode543.二叉树的直径

---
+++ LeetCode/Java/com/xuxin/solution/DiameterOfBinaryTree.java
@@ -0,0 +1,24 @@
+package com.xuxin.solution;
+
+import com.xuxin.util.TreeNode;
+
+public class DiameterOfBinaryTree {
+    int ans;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+
+    public int depth(TreeNode node) {
+        if (node == null) {
+            return 0;  // 访问到空节点了，返回0
+        }
+        int L = depth(node.left);
+        int R = depth(node.right);
+        ans = Math.max(ans, L + R + 1);
+        return Math.max(L, R) + 1;
+    }
+
+}
