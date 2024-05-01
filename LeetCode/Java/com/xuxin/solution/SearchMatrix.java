29b26f1dade34befee4ab9bf0143171ed3052a41
xuxin <2860543348@qq.com>
2024-05-01 23:15:27 +0800

    solution2. binary search

--- LeetCode/Java/com/xuxin/solution/SearchMatrix.java
+++ LeetCode/Java/com/xuxin/solution/SearchMatrix.java
@@ -2,6 +2,7 @@ package com.xuxin.solution;
 
 public class SearchMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
+        /* Solution1:
         if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
@@ -16,5 +17,31 @@ public class SearchMatrix {
             }
         }
         return false;
+        */
+
+        for (int i = 0; i < matrix.length; i++) {
+            int[] row = matrix[i];
+            if (target > row[row.length-1]) {
+                continue;
+            }
+            if (target < row[0]) {
+                return false;
+            }
+            int left = 0, right = row.length-1;
+            while (left <= right) {
+                int mid = left + (right-left)/2;
+                if (row[mid] == target) {
+                    return true;
+                } else if (row[mid] > target) {
+                    right = mid-1;
+                } else {
+                    left = mid+1;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        new SearchMatrix().searchMatrix(new int[][]{{1}, {3}}, 3);
     }
 }
