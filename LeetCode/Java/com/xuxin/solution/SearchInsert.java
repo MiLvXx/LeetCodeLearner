4270ce3d09183cbf9f4eeb45a7777ffa2c1aab93
xuxin <2860543348@qq.com>
2024-05-01 22:04:41 +0800

    Leetcode35.搜索插入位置

---
+++ LeetCode/Java/com/xuxin/solution/SearchInsert.java
@@ -0,0 +1,21 @@
+package com.xuxin.solution;
+
+/**
+ * Leetcode35.搜索插入位置
+ */
+public class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right + 1;
+    }
+}
