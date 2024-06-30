ab19f0448a22ba10848958f73b5160fca85dfc8b
xuxin <2860543348@qq.com>
2024-06-30 15:36:04 +0800

    optimize code

--- LeetCode/Java/com/xuxin/solution/SortList.java
+++ LeetCode/Java/com/xuxin/solution/SortList.java
@@ -4,50 +4,78 @@ import com.xuxin.util.ListNode;
 
 public class SortList {
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
+        return mergeSort(head);
+    }
+
+    /**
+     * 找到链表中点
+     * 
+     * @param head the head node
+     * @return
+     */
+    public ListNode findMid(ListNode head) {
+        if (head == null)
             return head;
-        }
+
         ListNode fast = head.next, slow = head;
         while (fast != null && fast.next != null) {
-            slow = slow.next;
             fast = fast.next.next;
+            slow = slow.next;
         }
-        ListNode tmp = slow.next;  //后半部分
-        slow.next = null;  // 切断链表
-        ListNode left = sortList(head);
-        ListNode right = sortList(tmp);
-        ListNode h = new ListNode(0);
-        ListNode res = h;
-        // 循环比较
-        while (left != null && right != null) {
-            if (left.val < right.val) {
-                h.next = left;
-                left = left.next;
+
+        return slow;
+    }
+
+    /**
+     * 拆分过程
+     * 
+     * @param head the head node
+     * @return
+     */
+    public ListNode mergeSort(ListNode head) {
+        // 当head.next==null时，说明当前链表只有一个元素
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = findMid(head);
+
+        ListNode next = mid.next;
+
+        mid.next = null; // 从中间结点断开，分别对两边mergeSort
+
+        ListNode left = mergeSort(head);
+        ListNode right = mergeSort(next);
+
+        return merge(left, right);
+    }
+
+    /**
+     * 合并过程
+     * 
+     * @param l1 the first node
+     * @param l2 the second node
+     * @return
+     */
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
             } else {
-                h.next = right;
-                right = right.next;
+                cur.next = l2;
+                l2 = l2.next;
             }
-            h = h.next;
+            cur = cur.next;
         }
-        h.next = left != null ? left : right;
-        return res.next;
+
+        if (l1 != null)
+            cur.next = l1;
+        if (l2 != null)
+            cur.next = l2;
+
+        return dummy.next;
     }
-    public static void main(String[] args) {
-        ListNode head = new ListNode(3);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(4);
-        ListNode node4 = new ListNode(6);
-        ListNode node5 = new ListNode(5);
-        ListNode node6 = new ListNode(1);
-        ListNode node7 = new ListNode(7);
-        ListNode node8 = new ListNode(8);
-        head.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        node6.next = node7;
-        node7.next = node8;
-        new SortList().sortList(head);
-    }
+
 }
