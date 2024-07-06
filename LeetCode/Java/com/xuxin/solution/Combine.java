6f7bd464b7fdfcde215ac9e3f5fc9da9c479aacd
xuxin <2860543348@qq.com>
2024-07-07 01:02:17 +0800

    remove redundant code

--- LeetCode/Java/com/xuxin/solution/Combine.java
+++ LeetCode/Java/com/xuxin/solution/Combine.java
@@ -14,28 +14,23 @@ public class Combine {
     private List<List<Integer>> res = new ArrayList<>();
 
     public List<List<Integer>> combine(int n, int k) {
-      
-        path = new ArrayList<>();
+
         dfs(n, k);
 
         return res;
     }
 
     private void dfs(int n, int k) {
-        int rest = k - path.size();  // 还要选 rest 个数
+        int rest = k - path.size(); // 还要选 rest 个数
         if (rest == 0) {
-            res.add(new ArrayList<>(path));  // 不能直接写 res.add(path) 
+            res.add(new ArrayList<>(path)); // 不能直接写 res.add(path)
             return;
         }
 
         for (int j = n; j >= rest; --j) {
             path.add(j);
-            dfs(j-1, k);
+            dfs(j - 1, k);
             path.remove(path.size() - 1);
         }
-    }
-
-    public static void main(String[] args) {
-        new Combine().combine(4, 2);
     }
 }
